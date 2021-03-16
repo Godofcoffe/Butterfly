b5f7655e220a3a3aae23f051b8e40eab3af2cad4
Kevin G. Tertuliano <robotnyte@gmail.com>
2021-03-16 12:46:13 -0300

    :pencil: mudado o idioma do programa

--- butterfly.py
+++ butterfly.py
@@ -5,7 +5,7 @@ from argparse import ArgumentParser, RawDescriptionHelpFormatter
 __currentDir__ = getcwd()
 not_dir = __currentDir__ + "\\Download"
 __version__ = '0.1.0-alpha'
-modulo_name = 'Butterfly: Baixa Videos, músicas ou Playlists.'
+modulo_name = 'Butterfly: Download Videos, Music or Playlists.'
 
 
 class Butterfly:
@@ -15,46 +15,46 @@ class Butterfly:
                                      description=f'{modulo_name} (versão {__version__})')
 
         self.parser.add_argument("string", action='store', metavar='STRINGS', nargs='+',
-                                 help='Um ou mais links para fazer download.'
-                                      ' Ponha o link entre aspas duplas "".'
-                                      ' Se for uma playlist o download só será feito se ela for pública.')
+                                 help='One or more links to download.'
+                                      '\n Enclose the link in double quotation marks "".'
+                                      '\n If it is a playlist, it will only be downloaded if it is public.')
 
-        self.parser.add_argument('--dir-dst', action='store', dest='diretorio',
+        self.parser.add_argument('--dir-dst', action='store', dest='path',
                                  default=not_dir, required=False, metavar='DIR',
-                                 help='Aqui é definido o diretório de destino do Download.'
-                                      ' Se não for especificado irá se criar uma pasta neste diretório chamada Downloads.')
+                                 help='Here, the download destination directory is defined.'
+                                      '\n If not specified, a folder will be created in this directory called "Downloads".')
 
         self.parser.add_argument('--define-ext', action='store', dest='ext',
                                  default="mp4", required=False, metavar='EXTENSION',
-                                 help='Define a extensão do arquivo final, mp3 ou mp4.'
-                                      ' O padrão é "mp4".')
+                                 help='Defines the extension of the final file mp3 or mp4.'
+                                      '\n The default is "mp4".')
 
         self.parser.add_argument('--define-resolution', action='store',
                                  default="720p", required=False, metavar='720p;480p;360p;240p;144p', dest='resol',
-                                 help='Define a resolução do(s) video(s) que será(m) baixado(s).'
-                                      ' O padrão é "720p".')
+                                 help='Defines the resolution of the video (s) to be downloaded.'
+                                      '\n The default is "720p".')
 
         self.parser.add_argument('--version', action='version',
-                                 version=f'%(prog)s {__version__}', help='Mostra a versão atual do programa.')
+                                 version=f'%(prog)s {__version__}', help='Shows the current version of the program.')
 
         self.args = self.parser.parse_args()
 
     def download_video(self):
         for strings in self.args.string:
             yt = YouTube(strings)
-            print(f'Baixando...: {yt.title}')
+            print(f'Downloading...: {yt.title}')
             try:
-                yt.streams.get_by_resolution(resolution=self.args.resol).download(self.args.diretorio,
+                yt.streams.get_by_resolution(resolution=self.args.resol).download(self.args.path,
                                                                                   skip_existing=True)
             except AttributeError:
-                print(f'Ocorreu um erro: O video foi mudando para a resolução máxima.')
-                yt.streams.get_highest_resolution().download(self.args.diretorio, skip_existing=True)
+                print(f'An error occurred: The video was changing to the maximum resolution.')
+                yt.streams.get_highest_resolution().download(self.args.path, skip_existing=True)
 
     def download_mp3(self):
         for strings in self.args.string:
             yt = YouTube(strings)
-            print(f'Baixando...: {yt.title}')
-            yt.streams.get_audio_only().download(self.args.diretorio, skip_existing=True)
+            print(f'Downloading...: {yt.title}')
+            yt.streams.get_audio_only().download(self.args.path, skip_existing=True)
 
     def download_playlist(self):
         for strings in self.args.string:
@@ -62,15 +62,15 @@ class Butterfly:
             print(videos)
             for vd in videos:
                 yt = YouTube(vd)
-                print(f'Baixando...: {yt.title}')
+                print(f'Downloading...: {yt.title}')
                 try:
                     if self.args.ext == 'mp4':
-                        yt.streams.get_by_resolution(resolution=self.args.resol).download(self.args.diretorio,
+                        yt.streams.get_by_resolution(resolution=self.args.resol).download(self.args.path,
                                                                                           skip_existing=True)
                     elif self.args.ext == 'mp3':
-                        yt.streams.get_audio_only().download(self.args.diretorio, skip_existing=True)
+                        yt.streams.get_audio_only().download(self.args.path, skip_existing=True)
                 except exceptions.RegexMatchError:
-                    print('A PlayList definida não é pública.')
+                    print('The defined PlayList is not public.')
 
     def main(self):
         urls = self.args.string
