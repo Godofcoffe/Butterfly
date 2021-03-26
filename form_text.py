bd77d99f14a06400d84ecfdb16ff32480d2a58d3
Kevin G. Tertuliano <robotnyte@gmail.com>
2021-03-26 19:32:10 -0300

    Update form_text.py

--- form_text.py
+++ form_text.py
@@ -1,17 +1,3 @@
-def style_text(s, txt):
-    """
-To edit the text font:
-     s = 'underline, negative or bold'
-     txt = 'text'
-    """
-    if s == 'bold':
-        return '\033[1m{}\033[m'.format(txt)
-    elif s == 'underline':
-        return '\033[4m{}\033[m'.format(txt)
-    elif s == 'negative':
-        return '\033[7m{}\033[m'.format(txt)
-
-
 def color_text(c, txt):
     """
 To edit the text color:
@@ -21,7 +7,7 @@ To edit the text color:
     if c == 'red':
         return '\033[31m{}\033[m'.format(txt)
     elif c == 'white':
-        return '\033[30m{}\033[m'.format(txt)
+        return '\033[97m{}\033[m'.format(txt)
     elif c == 'green':
         return '\033[32m{}\033[m'.format(txt)
     elif c == 'yellow':
@@ -32,25 +18,5 @@ To edit the text color:
         return '\033[36m{}\033[m'.format(txt)
     elif c == 'grey':
         return '\033[37m{}\033[m'.format(txt)
-
-
-def back_text(c, txt):
-    """
-To edit the text background:
-         c = 'color'
-         txt = 'text'
-    """
-    if c == 'red':
-        return '\033[41m{}\033[m'.format(txt)
-    elif c == 'white':
-        return '\033[40m{}\033[m'.format(txt)
-    elif c == 'green':
-        return '\033[42m{}\033[m'.format(txt)
-    elif c == 'yellow':
-        return '\033[43m{}\033[m'.format(txt)
-    elif c == 'blue':
-        return '\033[44m{}\033[m'.format(txt)
-    elif c == 'cyan':
-        return '\033[46m{}\033[m'.format(txt)
-    elif c == 'grey':
-        return '\033[47m{}\033[m'.format(txt)
+    elif c == 'magenta':
+        return '\033[35m{}\033[m'.format(txt)
